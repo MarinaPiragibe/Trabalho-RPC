119146138777c7c0406cd525adf1d88c5ba4b219
MarinaPiragibe <marinapiragibe@id.uff.br>
2023-04-29 11:40:59 -0300

    Implementa o contador de tempo de execução e resposta

--- scripts/cliente.py
+++ scripts/cliente.py
@@ -1,7 +1,9 @@
+import time
 import rpyc
 import sys
 from random import randint
 
+# Questão 4 em diante
 def criaVetor ():
 
     tamanho = int(input('Insira o tamanho do vetor: '))
@@ -18,14 +20,24 @@ if len(sys.argv) < 2:
 server = sys.argv[1]
 conn = rpyc.connect(server,18861)
 
-print(conn.root)
-print(conn.root.get_answer())
-print(conn.root.the_real_answer_though)
+# Questão 1
+# print(conn.root)
+# print(conn.root.get_answer())
+# print(conn.root.the_real_answer_though)
 
 clientVector = criaVetor()
 # print(clientVector, sum(clientVector))
+
+# Questões 5 em diante
+start = time.time()
+
+# Questão 4 em diante
 sumVector = conn.root.vectorSum(clientVector)
-print(sumVector)
+print(f'A soma dos elementos do vetor é {sumVector}')
+
+# Questões 5 em diante
+end = time.time()
+print(f'Tempo de resposta :{end-start}')
 
 
 # Questão 3
